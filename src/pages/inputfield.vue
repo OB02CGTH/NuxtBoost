17fba094d467c38f0fe597e9764e5b01c1cbbaf6
OB02CGTH <sornsiri1208@gmail.com>
2023-06-05 21:57:55 +0700

    Edit Input Field

--- src/pages/inputfield.vue
+++ src/pages/inputfield.vue
@@ -1,20 +1,20 @@
 <template>
   <section>
-    <o-field label="First name" labelClass="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-      <o-input inputClass="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  placeholder="John" v-model="name"></o-input>
+    <o-field rootClass="block mb-2 text-sm font-medium" label="First name">
+      <o-input inputClass="border block w-full p-2.5 text-sm rounded-lg" placeholder="John" v-model="name"></o-input>
     </o-field>
 
-    <o-field label="Password" labelClass="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-      <o-input type="password" value="iwantmytreasure" inputClass="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" password-reveal>
+    <o-field rootClass="block mb-2 text-sm font-medium" label="Password">
+      <o-input inputClass="border block w-full p-2.5 text-sm rounded-lg" type="password" value="iwantmytreasure" placeholder="•••••••••" password-reveal>
       </o-input>
     </o-field>
 
-    <o-field label="Email" variant="success" message="This email is invalid">
-      <o-input type="email" value="john@" maxlength="30"> </o-input>
+    <o-field rootClass="block mb-2 text-sm font-medium" variant="success" label="Email" message="This email is invalid">
+      <o-input inputClass="border block w-full p-2.5 text-sm rounded-lg"  type="email" value="john@" maxlength="30"> </o-input>
     </o-field>
     
-    <o-field label="Email" variant="danger" message="This email is invalid">
-      <o-input type="email" value="john@" maxlength="30"> </o-input>
+    <o-field rootClass="block mb-2 text-sm font-medium" variant="danger" label="Email" message="This email is invalid">
+      <o-input inputClass="border block w-full p-2.5 text-sm rounded-lg" type="email" value="john@" maxlength="30"> </o-input>
     </o-field>
   </section>
 </template>
@@ -46,12 +46,10 @@ export default defineComponent({
 
 <style>
   .o-input--success {
-    @apply bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500
+    @apply bg-green-50 border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-green-500
   }
   .o-input--danger {
-    @apply bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500
-  }
-  .o-input--primary {
-    @apply w-4 h-4 text-blue-600 bg-blue-100 border-blue-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-blue-800 focus:ring-2 dark:bg-blue-700 dark:border-blue-600
+    @apply bg-red-50 border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 dark:bg-gray-700 focus:border-red-500 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500
   }
 </style>
+<!-- border text-sm rounded-lg block w-full p-2.5 -->
